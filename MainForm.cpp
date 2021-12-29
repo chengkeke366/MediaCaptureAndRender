256650e8ef147f0306ad216e640efc5d196e503c
chengkeke <chengkeke@xdf.cn>
2021-12-29 23:52:51 +0800

    UI原型构建

---
+++ MainForm.cpp
@@ -0,0 +1,16 @@
+﻿#include "MainForm.h"
+#include "ui_MainForm.h"
+
+
+
+MainForm::MainForm(QWidget *parent) :
+	QWidget(parent),
+	ui(new Ui::MainForm)
+{
+	ui->setupUi(this);	
+}
+
+MainForm::~MainForm()
+{
+	delete ui;
+}
---
+++ MainForm.h
@@ -0,0 +1,28 @@
+﻿#ifndef FRMSWITCHBUTTON_H
+#define FRMSWITCHBUTTON_H
+
+#include <QWidget>
+#include <thread>
+#include <memory>
+#include <atomic>
+
+namespace Ui
+{
+class MainForm;
+}
+
+class AVFormatContext;
+
+
+class MainForm : public QWidget
+{
+	Q_OBJECT
+
+public:
+	explicit MainForm(QWidget *parent = 0);
+	~MainForm();
+private:
+	Ui::MainForm*ui;
+};
+
+#endif // frmSwitchButton_H
